7544c3e296053056730c732f9da7fd27a0285d6a
Faraheng <farahengnajib@gmail.com>
2020-09-26 00:54:24 +0800

    add association

--- models/user.js
+++ models/user.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      user.belongsToMany(models.note, { through: "notes", foreignKey: "userid" });
+    
+    return user;
     }
   };
   user.init({
