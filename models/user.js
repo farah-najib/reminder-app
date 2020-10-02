58e10dc9c9c8962c40d7d622dedd1da2d9ec4cda
Faraheng <farahengnajib@gmail.com>
2020-10-02 12:12:45 +0800

    txt

--- models/user.js
+++ models/user.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+
+      user.hasMany(models.reminder, { 
+        foreignKey: 'userId', 
+        as: 'reminder' 
+      });
      
     }
   };
