0545b802b6f426b913ea36f947438fac8e5b5f86
FarahNajib <farahengnajib@gmail.com>
2020-11-22 00:00:22 +0800

    add front end

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const jsxElement = <h1>This is a JSX element</h1>
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(jsxElement, rootElement)
