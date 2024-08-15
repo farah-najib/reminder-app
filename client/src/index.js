1741dd774f38af902fdbe73fd07558e9c3c7262b
Farah Najib <farahengnajib@gmail.com>
2024-08-15 20:00:48 +0200

    remove frontend part to rebuild again

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-const jsxElement = <h1>This is a JSX element</h1>
-const rootElement = document.getElementById('root')
-
-ReactDOM.render(jsxElement, rootElement)
